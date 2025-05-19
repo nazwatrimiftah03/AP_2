36e1f3db2e2b12a588d1e5da3f3667a86f6c0ddf
nazwatrimiftah03 <nazwatrimiftah03@gmail.com>
2025-05-19 21:44:09 +0700

    Update belajar.cpp

--- PERTEMUAN 1/belajar.cpp
+++ PERTEMUAN 1/belajar.cpp
@@ -1,46 +1,42 @@
-#include <iostream> //header untuk c++
+#include <iostream>  // header untuk C++
 #include <conio.h> // header untuk fungsi getch() dan getche()
 using namespace std;
 
 int main() {
-
-    string nama;
-    char KOM,jenisKelamin;
-    int NIM;
-    float IP;
     
-    /* ini untuk komen 
+    string nama;
+    char kom, jenisKelamin;
+    int nim;
+    float ip;
+
+    /*ini untuk komen 
     beberapa baris*/
 
-    cout << "Hello world" << endl;
+    cout << "Hello World" << endl; // menampilkan teks "Hello World" ke layar
 
-    cout << "Masukkan nama Anda : ";
-    cin.ignore();  
-    getline(cin, nama);
+    cout << "Masukkan nama Anda : "; //membaca input nama dari pengguna termasuk spasi
+    // cin >> nama;
+    getline(cin, nama); // agar karakter spasi bisa terbaca
 
-    cout << "Masukkan KOM Anda : ";
-    cin >> KOM;
+    cout << "Masukkan KOM : "; // membaca input kom
+    cin >> kom;
 
-    cout << "Masukkan IP Anda : ";
-    cin >> IP;
+    cout << "Masukkan NIM : "; //membaca input nim
+    cin >> nim;
 
-    cout << "Masukkan NIM Anda : ";
-    cin >> NIM;
+    cout << "Masukkan IP : "; // membaca input ip
+    cin >> ip;
 
-    cout << "masukkan jenis kelamin (L/P): ";
-    jenisKelamin = getche(): 
+    cout << "Masukkan jenis kelamin (L/P) :  ";
+    jenisKelamin = getche(); // agar karakter langsung tampil, tanpa tekan enter
 
-    /*untuk output*/
-    cout << "\nData yang Anda masukkan:" << endl;
-    cout << "Nama : " << nama << endl;
-    cout << "KOM  : " << KOM << endl;
-    cout << "IP   : " << IP << endl;
-    cout << "NIM  : " << NIM << endl;
+    /*Untuk Output*/
+    cout << "\n" << nama << endl; // menampilkan nama yg di input
+    cout << nim << endl;// menampilkan NIMama yg di input
+    cout << kom << endl;// menampilkan KOM yg di input
+    cout << ip << endl;// menampilkan IP yg di input
     putchar(jenisKelamin); // untuk menampilkan karakter
-    
-    getch(); //karakter yang di ketik
 
-    return 0;
+    getch(); // karakter yang diketik ga di tampilkan di layar
 }
-
 
