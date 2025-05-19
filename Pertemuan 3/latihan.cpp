128c891a6238a64baad0cdeffe22917080a5ca12
nazwatrimiftah03 <nazwatrimiftah03@gmail.com>
2025-05-19 22:18:09 +0700

    latihan.cpp

--- Pertemuan 3/latihan.cpp
+++ Pertemuan 3/latihan.cpp
@@ -1,20 +1,20 @@
-#include <iostream>
+#include <iostream> // header untuk c++
 using namespace std;
 
 int main() {
-    int bilangan;
-    system("cls");
+    int angka; // deklarasi variabel
+    system("cls"); //membersihkan layar
     
     cout << "Masukkan sebuah bilangan bulat: ";
-    cin >> bilangan;
+    cin >> angka; //inputan bil.bulat dan menyimpan di var.angka
     
-    if (bilangan % 5 == 0 && bilangan % 10 == 0) {
-        cout << bilangan << " adalah kelipatan dari 5 dan 10." << endl;
-    } else if (bilangan % 5 == 0) {
-        cout << bilangan << " adalah kelipatan dari 5." << endl;
-    } else if (bilangan % 10 == 0) {
-        cout << bilangan << " adalah kelipatan dari 10." << endl;
-    } else {
-        cout << bilangan << " bukan kelipatan dari 5 atau 10." << endl;
+if (angka % 10 == 0) { //memeriksa apakah angka habis dibagi 10
+        cout << angka << " adalah kelipatan dari 10." << endl;
+    } else if (angka % 5 == 0) { // Jika kondisi pertama salah, periksa apakah 'angka' habis dibagi 5
+        cout << angka << " adalah kelipatan dari 5." << endl; / Jika ya, tampilkan pesan ini
+    } else { // Jika kedua kondisi di atas salah
+        cout << angka << " bukan kelipatan dari 5 atau 10." << endl; // tampilan
     }
+
+ return 0;
 }
