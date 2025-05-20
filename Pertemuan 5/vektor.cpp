779ef2587baa0603f5061c509db4b91efd62d0a9
nazwatrimiftah03 <nazwatrimiftah03@gmail.com>
2025-05-20 12:57:38 +0700

    Update vektor.cpp

--- Pertemuan 5/vektor.cpp
+++ Pertemuan 5/vektor.cpp
@@ -1,45 +1,35 @@
-#include <iostream>
-#include <vector>
+#include <iostream> //header untuk c++
+#include <vector> // header untuk vektor
 
 using namespace std;
 
-int main() {
+int main (){
     // Vector Declaration & Initialization
-    vector<string> nama_karyawan = {"alya", "parul", "syukron", "aurick", "dzakwan"};
+    vector<string> nama_kayawan = {"Alya", "Parul", "Syukron", "Aurick", "Dzakwan"}; // Inisialisasi vector string
 
-    // Menampilkan elemen secara ascending 
-    cout << "Daftar Karyawan:" << endl;
-    for (int i = 0; i < nama_karyawan.size(); i++) {
-        cout << nama_karyawan[i] << endl;
+    // Ascending Element of Vector
+    // for(int i = 0; i < nama_kayawan.size(); i++) { // Loop melalui elemen vector menggunakan indeks.
+    //     cout << nama_kayawan[i] << endl;
+    // }
+
+    // for (string karyawan : nama_kayawan) { // Range-based for loop untuk setiap elemen
+    //     cout << karyawan << endl;
+    // }
+
+
+    // Add data to Vector
+    nama_kayawan.push_back("Imam"); // menambah elemen ke akhir vektor
+
+    // for(int i = 0; i < nama_kayawan.size(); i++) { // Loop untuk menampilkan vector setelah penambahan
+    //     cout << nama_kayawan[i] << endl;
+    // }
+
+    
+    // Delete data from Vector
+    nama_kayawan.pop_back(); // Menghapus elemen terakhir dari vector
+    nama_kayawan.erase(nama_kayawan.begin() + 3); // Menghapus elemen pada indeks 3
+
+    for(int i = 0; i < nama_kayawan.size(); i++) { // Loop untuk menampilkan vector setelah penghapusan
+        cout << nama_kayawan[i] << endl;
     }
-
-    // Menampilkan elemen secara ascending 
-    cout << "Daftar Karyawan " << endl;
-    for (string karyawan : nama_karyawan) {
-        cout << karyawan << endl;
-    }
-
-    // Menambahkan data ke vector
-    nama_karyawan.push_back("Imam");
-
-    // Menampilkan seluruh isi setelah penambahan
-    cout << "Setelah Menambahkan 'Imam':" << endl;
-    for (int i = 0; i < nama_karyawan.size(); i++) {
-        cout << nama_karyawan[i] << endl;
-    }
-
-    // Menghapus elemen terakhir
-    nama_karyawan.pop_back();
-
-    // Menghapus elemen ke-4
-    if (nama_karyawan.size() > 3) {
-        nama_karyawan.erase(nama_karyawan.begin() + 3);
-    }
-
-    // Menampilkan hasil akhir setelah penghapusan
-    cout << "Setelah Penghapusan:" << endl;
-    for (int i = 0; i < nama_karyawan.size(); i++) {
-        cout << nama_karyawan[i] << endl;
-}
-
 }
