33b13378e4f2b2f40c136ea54ea2288b124e2d41
nazwatrimiftah03 <nazwatrimiftah03@gmail.com>
2025-05-20 12:23:59 +0700

    latihan1.cpp

--- Pertemuan 4/loop.cpp/loop.cpp/Pertemuan 4/latihan1.cpp
+++ Pertemuan 4/loop.cpp/loop.cpp/Pertemuan 4/latihan1.cpp
@@ -1,21 +1,21 @@
-#include <iostream>
+#include <iostream> // header untuk c++
 using namespace std;
 
 int main() {
-    string kalimat;
-    int i;
+    string kalimat; //deklrasi variabel string
+    int i; //deklrasi var.integer untuk perulangan
 
     system("CLS");
 
-    cout << "masukkan kalimat :";
-    getline (cin, kalimat);
+    cout << "masukkan kalimat :"; // menampilkan pesan inputan
+    getline (cin, kalimat);// membaca seluruh baris inputan
 
     //  n a z w a
-    for(i = 0; i< kalimat.length(); i++) {
-        kalimat[i] = toupper(kalimat[i]);
+    for(i = 0; i< kalimat.length(); i++) { // perulangan untuk setiap karakter
+        kalimat[i] = toupper(kalimat[i]); //mengubah setiap karakter menjadi huruf kapital
     }
 
-    cout <<"kalimat dalam huruf kapital :" <<kalimat << endl;
+    cout <<"kalimat dalam huruf kapital :" <<kalimat << endl; //menampilkan kalimat dalam huruf kapital
     
 
 }
