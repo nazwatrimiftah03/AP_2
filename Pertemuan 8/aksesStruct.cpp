af880718e89183012045ce1a5a9579d0d41e91c6
nazwatrimiftah03 <nazwatrimiftah03@gmail.com>
2025-05-20 20:58:38 +0700

    AksesStruct.cpp

--- Pertemuan 8/aksesStruct.cpp
+++ Pertemuan 8/aksesStruct.cpp
@@ -1,17 +1,17 @@
-#include <iostream>
-#include <string>
+#include <iostream> //header untuk c++
+#include <string> //untuk tipe data string
 using namespace std;
 
 struct mahasiswa{
-    string nama;
-    int umur;
-    float ipk;
+    string nama; //string nma mahasiswa
+    int umur;//umur mahasiswa
+    float ipk; //ipk mahasiswa
 };
 
 int main(){
     
-    mahasiswa mhs1;
-
+    mahasiswa mhs1; // objek mahasiswa
+// data mahasiswa operator (.) karna variabel langsung
     mhs1.nama="nazwa";
     mhs1.umur= 19;
     mhs1.ipk = 4.00;
@@ -19,8 +19,10 @@ int main(){
     // cout<< "nama = "  << mhs1.nama<<endl;
     // cout<< "umur = "  << mhs1.umur<<endl;
     // cout<< "ipk = "  << mhs1.ipk<<endl;
-
+    
+// Bikin pointer yang nunjuk ke alamat memori mhs1
     mahasiswa *ptrMhs= &mhs1;
+// Akses data mhs1 LEWAT POINTER pakai operator panah (->)
     cout <<"nama=" << ptrMhs -> nama << endl;
     cout <<"umur=" << ptrMhs -> umur << endl;
     cout <<"ipk=" << ptrMhs -> ipk << endl;
