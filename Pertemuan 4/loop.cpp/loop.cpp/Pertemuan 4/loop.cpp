1a7008eb9cbb25b77b10f9ac328a09d71769e993
nazwatrimiftah03 <nazwatrimiftah03@gmail.com>
2025-05-20 12:39:37 +0700

    loop.cpp

--- Pertemuan 4/loop.cpp/loop.cpp/Pertemuan 4/loop.cpp
+++ Pertemuan 4/loop.cpp/loop.cpp/Pertemuan 4/loop.cpp
@@ -1,69 +1,69 @@
-#include <iostream>
+#include <iostream> // header untuk c++
 using namespace std;
 
-int main () {
-    // goto
-    // Hello world,Fasilkom-TI,Ilmu Komputer,IKLC
-    // -> Hello world, IKLC, Ilmu Komputer,Fasilkom-TI
+int main() {
+    // goto label
+    // Hello World, Fasilkom-TI, Ilmu Komputer, IKLC
+    // -> Hello World, IKLC, Ilmu Komputer, Fasilkom-TI
 
     // a:
-    // cout << "Hello world" << endl;
-    // goto d;
+    //     cout << "Hello World" << endl;
+    //     goto d;
     // b:
-    // cout << "Fasilkom-TI" << endl;
-    // return 0;
+    //     cout << "Fasilkom-TI" << endl;
+    //     return 0;
     // c:
-    // cout << "Ilmu Komputer" << endl;
-    // goto b;
+    //     cout << "Ilmu Komputer" << endl;
+    //     goto b;
     // d:
-    // cout << "IKLC" << endl;
-    // goto c;
+    //     cout << "IKLC" << endl;
+    //     goto c;
 
     // menampilkan bilangan genap 10 -> 2 dengan goto
-    // 
-    
+    // int i = 10;
+    // genap:
+    // if (i % 2 == 0) {
+    //     cout << i << " ";
+    // } i--;  
 
+    // if (i >= 2) {
+    //     goto genap;
+    // }
 
-
-
-
-
-
-
-    // statement while
+    // Statement While
     // int i = 1;
-    // while (i <= 10){
-    //     if ( i% 2 == 0) {
+    // while (i <= 10) {
+    //     if (i % 2 == 0) {
     //         cout << i << " ";
     //     } i++;
     // }
 
-    // statement do-while
+    // Statement Do-While
     // int i = 1;
     // do {
     //     cout << i << endl;
-    // } while (i <= 0);
+    // } while (i <= 0); // 1 <= 0 // kondisi false, loop jalan sekali
 
-    // statement for
+    // Statement for
     // for (inisialisasi, kondisi, increase)
-    // for (int i= 1; i <= 10; i+=2) {
-    //     cout << "Hello World" << endl; i = i + 2
+    // for (int i = 1; i <= 10; i+=2) {
+    //     cout << "Hello World" << endl; // i = i + 2 //cetak hello wordl 5 kali
     // }
-
+    
     // Nested for
     // ***** 5 x 5
-    // for (int i = 1; i <=5; i++) {
-    //     for (int j = 1; j <= 5; j++) {
-    //         cout << "*";
+    // for (int i = 1; i <= 5; i++) { // loop baris
+    //     for (int j = 1; j <= 5; j++) { // loop kolom
+    //         cout << "* ";
     //     }
-    //     cout << endl;
+    //     cout << endl; // baris baru
     // }
 
     // segitiga siku-siku
-    // for(int i = 1; i<=5; i++){
-    //     for (int j = 1; j <= i; j++) {
-    //         cout << "*";
+    // for(int i = 1; i <= 5; i++) { // loop baris
+    //     for (int j = 1; j <= i; j++){ // loop kolom sesuai baris
+    //         cout << "* ";
     //     }
-    //     cout << endl;
+    //     cout << endl; // baris baru
     // }
 }
