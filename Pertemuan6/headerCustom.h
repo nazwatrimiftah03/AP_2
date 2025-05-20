1fd288c0eaa88a9404cf0b4663aeef9cbe1fe348
nazwatrimiftah03 <nazwatrimiftah03@gmail.com>
2025-05-20 20:29:53 +0700

    headerCustom.h

--- Pertemuan6/headerCustom.h
+++ Pertemuan6/headerCustom.h
@@ -1,3 +1,3 @@
-#define L <<
-#define u using namespace std;
-#define o cout
+#define L << // Macro untuk operator insertion
+#define u using namespace std; // Macro untuk mempersingkat 'using namespace std;'
+#define o cout // Macro untuk mempersingkat 'cout'
