f47a1a4e851e348f8e2280ee574f2dcd71a66792
nazwatrimiftah03 <nazwatrimiftah03@gmail.com>
2025-05-21 20:33:48 +0700

    Delete Pertemuan 6 directory

--- Pertemuan 6/headerCustom.cpp
+++
@@ -1,3 +0,0 @@
-#define L << // Macro untuk operator insertion
-#define u using namespace std; // Macro untuk mempersingkat 'using namespace std;'
-#define o cout // Macro untuk mempersingkat 'cout'
